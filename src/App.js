1edfe412e735438e5089efd992f7c71fe8a9fb82
Octonox <victorpetit40gmail.com>
2021-08-19 22:15:46 +0200

    Fucking trying to build those motherfucking random flying characters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class RandomChar extends React.Component {
+  render () {
+    var posX = Math.floor(Math.random() * (this.props.maxX + 1));
+    var posY = Math.floor(Math.random() * (this.props.maxY + 1));
+
+    const styling = "bottom: " + String(posX) + ";left: " + String(posY);
+
+    return (
+      <p style={styling}>{this.props.text}</p>
+    );
+  }
+}
+
+class FlyingChars extends React.Component {
+  render () {
+    const tab = [
+      "a", "z", "e", "r", "t", "t", "y", "u", "i", "o",
+      "p", "q", "s", "d", "f", "g", "h", "j", "k", "l",
+      "m", "w", "x", "c", "v", "b", "n"
+    ];
+
+    return (
+      {
+        tab.map((char) => (
+          <RandomChar text={char}></RandomChar>
+        ))
+      }
+    );
+  }
+}
+
+class Header extends React.Component {
+  render () {
+    return (
+      <header>
+        <div className="headerFlex">
+          <div className="title">
+            <h2><span>Agora</span></h2>
+          </div>
+          <div className="headerBuf"></div>
+        </div>
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class CentralPanel extends React.Component {
+  render () {
+    return (
+      <div className="gridPanel">
+        <FlyingChars nb="200"></FlyingChars>
+        <div className="welcomeText">
+          <p>The power of</p>
+          <p><span>Words</span></p>
+          <p><span>Thinking</span></p>
+          <p><span>Facts</span></p>
+        </div>
+      </div>
+    );
+  }
+}
+
+class HomePage extends React.Component {
+  render () {
+    return (
+      <div className="bodyFlex">
+        <Header></Header>
+        <CentralPanel></CentralPanel>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render () {
+    return (
+      <HomePage></HomePage>
+    );
+  }
 }
 
 export default App;
