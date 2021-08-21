496c2abe203f55eb159be8ca1e66b2a7776c945b
Octonox <victorpetit40gmail.com>
2021-08-21 18:51:56 +0200

    Implemented the move method.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { getDefaultNormalizer } from '@testing-library/react';
 
 class PlaneVect {
+  constructor(constant, otherVect) {
+    this.x = constant * otherVect.x();
+    this.y = constant * otherVect.y();
+  }
+
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -28,6 +34,11 @@ class PlaneVect {
 /* Returns the subtraction of the two vectors */
 function subVect(vectA, vectB) {
   return new PlaneVect(vectA.x() - vectB.x(), vectA.y() - vectB.y())
+}
+
+/* Returns the distance between two points A and B */
+function norme(pointA, pointB) {
+  return Math.sqrt((pointA.x() - pointB().x())^2 + (pointA.y() - pointA.x())^2)
 }
 
 /* Returns the direction of the vector between A and B (its angle with the x-axis) */
@@ -62,18 +73,49 @@ class Header extends React.Component {
   }
 }
 
+class FieldPoint {
+  constructor(maxMass, maxX, maxY) {
+    this.mass = Math.random() * maxMass;
+    this.position = new PlaneVect(Math.random() * maxX, Math.random() * maxY);
+  }
+
+  mass() {
+    return this.mass
+  }
+
+  position() {
+    return this.position
+  }
+}
+
+/* Returns the force vector between A and B */
+function NewtonLaw(G, mass, A, B) {
+  return PlaneVect(G * mass^2 / norme(A, B)^3, subVect(B, A))
+}
+
 // Represents a random character that will move on its own
 class RandomChar extends React.Component {
   constructor (props) {
     super(props);
+    const bufX = Math.random() * (this.props.maxX + 1);
+    const bufY = Math.random() * (this.props.maxY + 1);
+    const bufPosition = new PlaneVect(bufX, bufY)
+    const bufFieldPoint = new FieldPoint(this.props.maxMass, this.props.maxX + 1, this.props.maxY + 1);
+
     /* The states variables will be the acceleration, the velocity and the position of the character */
     /* velx and vely are the maximum velocity possible */
     /* Field point is the random point which is going to apply a force on the character, and thus an acceleration */
+    /* Explanation on the acceleration:
+          We use here newton laws of gravitation to calculate the acceleration induced by the field point
+          So basically this tough formula is just the vectorial application of this law
+          We multiply the unity vector (AB/norme(AB)) by G*mass^2/norme(AB)^2 
+          G is chosen by us as a property, it is by changing its value that we'll change the ways characters move */
+
     this.state =  {
-      fieldPoint = new PlaneVect(Math.random() * (this.props.maxX + 1), Math.random() * (this.props.maxY + 1))
-      ,
+      fieldPoint = bufFieldPoint,
+      acceleration = NewtonLaw(this.props.G, bufFieldPoint.mass(), bufPosition, bufFieldPoint.position()),
       velocity = new PlaneVect(Math.random() * this.props.velx, Math.random() * this.props.vely),
-      position = new PlaneVect(Math.random() * (this.props.maxX + 1), Math.random() * (this.props.maxY + 1))
+      position = bufPosition
     };
   }
 
@@ -95,9 +137,34 @@ class RandomChar extends React.Component {
     const toAddY = hypothenus * Math.sin(direction); */
 
     /* What we want is that the char has an acceleration, and an initial velocity and position,
-    and each time move() is called, the char has a chance to undergo a new force, and hence a new acceleration,
+    and each time move() is called, the char has a chance (maybe one out of ten) to undergo a new force, and hence a new acceleration,
     which means its velocity will change, depending on the laws of kinematics. It is like programming a particle
     that flies in a magnetic field */
+
+    // First is decided whether a new FieldPoint is applied or not 
+    if (Math.random() <= 0.1) {
+      // In this case we create a new field point and calculate the new acceleration
+      newFieldPoint = new FieldPoint(this.props.maxMass, this.props.maxX, this.props.maxY);
+
+      // We change the value of the state variable acceleration
+      this.setState({
+        fieldPoint: newFieldPoint,
+        acceleration: NewtonLaw(this.props.G, newFieldPoint.mass(), this.state.position(), newFieldPoint.position())
+      });
+    }
+
+    // If there was a new field point created, the character will undergo a new acceleration, which will change its velocity and position
+    // Now, the character must be moved
+    // The speed changes the position, and the acceleration changes the speed. Finally, the new position changes the acceleration, because the vector between the point and the field point is not the same
+    const newPosition = new PlaneVect(this.state.position.x() + this.state.velocity.x(), this.state.position.y() + this.state.velocity.y());
+    const newVelocity = new PlaneVect(this.state.velocity.x() + this.state.acceleration.x(), this.state.velocity.y() + this.state.acceleration.y());
+    const newAcceleration = NewtonLaw(this.props.G, this.state.fieldPoint.mass(), newPosition, this.state.fieldPoint);
+
+    this.setState({
+      position: newPosition,
+      velocity: newVelocity,
+      acceleration: newAcceleration
+    });
   }
 
   componentWillUnmount () {
@@ -120,7 +187,7 @@ class FlyingChars extends React.Component {
       <ul className="no-bullets">
       {
         tab.map((ch) => ( 
-          <RandomChar text={ch} maxX={1400} maxY={1000} velx={} vely={}></RandomChar> // TO CHANGE THE SQUARE IN WHICH THEY CAN MOVE
+          <RandomChar text={ch} maxMass={} maxX={1400} maxY={1000} velx={} vely={} G={}></RandomChar> // TO CHANGE THE SQUARE IN WHICH THEY CAN MOVE
         ))
       }
       </ul>
